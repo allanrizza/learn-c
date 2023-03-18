9c3fd405a3ce0827b0bfa4c0d6e1d21bb614ca09
Abdullah <allanrizza.dev@gmail.com>
2023-03-18 00:55:10 -0300

    first commit

---
+++ variables-and-types/arrays.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+int main() {
+
+    // Arrays can only have one type of variable, because they are implemented as a sequence of values in the computer's memory. Because of that, accessing a specific array cell is very efficient.
+
+    int numbers[10];
+
+    numbers[0] = 19;
+    numbers[1] = 32;
+    numbers[2] = 45;
+    numbers[3] = 47;
+    numbers[4] = 51;
+    numbers[5] = 64;
+    numbers[6] = 88;
+
+    printf("The 7th number in the array is %d", numbers[6]);
+}
---
+++ variables-and-types/variables-and-types.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int main() {
+    int x = 15;
+    float y = 27.32;
+    float sum = x+y;
+    printf("The sum between %i and %f is %f.", x, y, sum);
+}
