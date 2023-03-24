91de7c91cb21d4ae03433119a73e111e040cd313
Allan Rizza <allanrizza.dev@gmail.com>
2023-03-23 21:20:45 -0300

    learning strings concatenation

--- strings.c
+++ strings.c
@@ -14,8 +14,18 @@ int main() {
     printf("string 2 length: %d\n", strlen(str2));
 
     if(strncmp(str1, str2, 4) == 0) {
-        printf("comparison returns true");
+        printf("comparison returns true\n");
     } else {
-        printf("comparison returns false");
+        printf("comparison returns false\n");
     }
+
+    char first[20]="String ";
+    char second[20]="Concatenation";
+
+    strncat(first, second, 3);
+    printf("%s\n", first);
+
+    strncat(first, second,20);
+    printf("%s\n",first);
+
 }
